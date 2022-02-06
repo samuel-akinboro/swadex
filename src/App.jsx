1194e186ebac2c96d17fdf952b02dbebbed339e7
Akinboro Samuel Oluwasegun <73400495+victor-akinboro@users.noreply.github.com>
2022-02-06 15:47:47 -0800

    connect wallet design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Home from './Pages/Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CryptoolyLogin from './Pages/CryptoolyLogin';
+import SelectAccount from './Pages/SelectAccount';
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login/cryptooly" element={<CryptoolyLogin />} />
+          <Route path="/login/cryptooly/select-account" element={<SelectAccount />} />
         </Routes>
       </BrowserRouter>
     </div>
